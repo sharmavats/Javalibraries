b9b08fa46d9884b2e4515732b339cab022e4be7d
sharmavats <kamal251199@gmail.com>
2021-12-27 22:53:06 +0530

    lambda expression inside collection

---
+++ Javalambdaoperations/lambdaexpinsidecollection.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class lambdaexpinsidecollection {
+    public static void main(String[] args) {
+        List<Integer> a1 = Arrays.asList(10,20,14,15,17);
+        a1.forEach(x-> System.out.println(x));
+
+    }
+}
