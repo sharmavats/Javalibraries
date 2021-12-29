22fc8b5fef3c2df37d5a2526e216109354a41faa
sharmavats <kamal251199@gmail.com>
2021-12-29 23:20:19 +0530

    for loop arraylist

---
+++ Arraylist/forlooparraylist.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class forlooparraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        for(int i=0; i<100; i++){
+            al.add(i);
+        }
+        System.out.println(al);
+    }
+}
