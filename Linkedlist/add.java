577b4521091b81c4978b42d4c78779ebe06c15f4
sharmavats <kamal251199@gmail.com>
2022-01-01 23:53:05 +0530

    add method

---
+++ Linkedlist/add.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class add{
+    public static void main(String[] args) {
+        LinkedList<Integer> l = new LinkedList<Integer>();
+        l.add(10);
+        l.add(20);
+        l.add(30);
+        l.add(40);
+        System.out.println(l);
+    }
+}
