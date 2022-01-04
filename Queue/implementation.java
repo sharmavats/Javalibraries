55807e02026bff3cbfae881504a11688cfa4b447
sharmavats <kamal251199@gmail.com>
2022-01-04 23:46:31 +0530

    implementation of queue

---
+++ Queue/implementation.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class implementation{
+    public static void main(String[] args) {
+        //Queue<Integer> q = new LinkedList<Integer>();
+        Queue<Integer> q = new ArrayDeque<Integer>();
+        q.offer(10);
+        q.offer(20);
+        q.offer(30);
+        System.out.println(q.isEmpty());
+        System.out.println(q.size());
+        System.out.println(q);
+    }
+}
