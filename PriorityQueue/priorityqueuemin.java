4311072e983cba802ce6d54004584cddf587ed4e
sharmavats <kamal251199@gmail.com>
2022-01-04 23:53:07 +0530

    priority queue min heap implementation

---
+++ PriorityQueue/priorityqueuemin.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class priorityqueuemin{
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        pq.add(10);
+        pq.add(20);
+        pq.add(30);
+        System.out.println(pq.peek());
+        System.out.println(pq.poll());
+        System.out.println(pq.peek());
+    }
+}
