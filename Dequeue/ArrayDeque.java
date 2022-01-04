7c27ef9004088540658aae1277d571729ad3c2d1
sharmavats <kamal251199@gmail.com>
2022-01-04 23:48:55 +0530

    arraydeque

---
+++ Dequeue/ArrayDeque.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.ArrayDeque;
+public class ArrayDeque {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> d = new ArrayDeque<Integer>();
+        d.add(10);
+        d.add(20);
+        d.add(30);
+        System.out.print(" "+d);
+    }
+}
