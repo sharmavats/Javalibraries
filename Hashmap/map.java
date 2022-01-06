1942981e7bededf06b28865a4dc5f898348861cd
sharmavats <kamal251199@gmail.com>
2022-01-06 23:55:28 +0530

    map implementation

---
+++ Hashmap/map.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class map{
+    public static void main(String[] args) {
+        //creating instance of hashmap
+        Map<Integer,String> m = new HashMap<Integer,String>();
+        m.put(20009,"kamal");
+        m.put(20010, "himanshu");
+        m.put(20011,"parshant");
+        System.out.println(m);
+        
+    }
+}
