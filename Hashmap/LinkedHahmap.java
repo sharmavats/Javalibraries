22b62f1ba790b2656771f77875a4117be6c947e2
sharmavats <kamal251199@gmail.com>
2022-01-06 23:58:28 +0530

    linkedhashmap

---
+++ Hashmap/LinkedHahmap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class LinkedHahmap {
+    public static void main(String[] args) {
+        LinkedHashMap<Integer,String> m = new LinkedHashMap<Integer,String>();
+        m.put(1, "kamal");
+        m.put(2,"yash");
+        m.put(3,"ravinder");
+        m.put(4,"babita");
+        System.out.println(m);
+        
+    }
+    
+}
