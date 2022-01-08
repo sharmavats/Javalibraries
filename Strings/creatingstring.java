978e3801ab45ae382d0de6928ca5781ff93e6a9b
sharmavats <kamal251199@gmail.com>
2022-01-08 23:45:50 +0530

    creating string

---
+++ Strings/creatingstring.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class creatingstring{
+    public static void main(String[] args){
+    char arr[] = {'j','a','v','a'}; //character array
+    System.out.println(arr);
+    String str = ("java");
+    System.out.println(str);
+    String s = new String("java");
+    System.out.println(s);
+    StringBuffer s1 = new StringBuffer("java");
+    System.out.println(s1);
+    StringBuilder s2 = new StringBuilder("java");
+    System.out.println(s2);
+    }
+}
