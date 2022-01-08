87541587f16d788eb83218bd46ac2d13f2677741
sharmavats <kamal251199@gmail.com>
2022-01-08 23:46:56 +0530

    methods of string

---
+++ Strings/methods.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class methods{
+public static void main(String[] args){
+String str = "Geeks for Geeks";
+String s = "Geeks ";
+System.out.println(str.length());
+System.out.println(str.charAt(3));
+System.out.println(str.substring(3));
+System.out.println(str.substring(3,6));
+System.out.println(str.contains(s));
+System.out.println(str.equals(s));
+System.out.println(str.compareTo(s));
+System.out.println(str.indexOf(s));
+System.out.println(str.indexOf(s,1));// returns the first occurence of the s in str after index 1
+System.out.println(str.concat(s));// or str + s
+System.out.println(str+s);
+System.out.println(str+s);
+
+
+
+    
+
+
+}
+}
